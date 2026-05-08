d76889d301c79120786b9ab81ff4b2da18de66d6
NombreUsuario <correo@gmail.com>
2026-05-08 11:03:41 +0200

    consultas hechas, a falta de las de modificar, eliminar e insertar de las que esta solamente el esqueleto

--- AlojamientosDavidMartinEF/AlojamientosDavid/CapaDatos/Gestion.cs
+++ AlojamientosDavidMartinEF/AlojamientosDavid/CapaDatos/Gestion.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Entities;
 using BuscarServidor;
+using System.Runtime.Remoting.Messaging;
 
 namespace CapaDatos
 {
@@ -21,5 +22,59 @@ namespace CapaDatos
             gestionar = new AlojamientosDavidMartinEntities(cadenaConexion); // Modificar el constructor de EstadoEntities y los nombres de la Base de datos, modelo, …
         }
         //Al crear los modelos en entities me he equivocado y les he puesto de nombre ModelEstado como en el ejercicio anterior, lo digo porque puede haber confusion
+
+        public List<CLIENTE>devolverClientes(){
+            return gestionar.CLIENTES.OrderBy(cl=>cl.NOMBRE).ToList();
+            }
+        public List<ESTABLECIMIENTO> devolverEstablecimientos()
+        {
+            return gestionar.ESTABLECIMIENTOS.ToList();
+        }
+        public List<UNIDADES_ALOJAMIENTO> devolverUnidadesEstablecimiento()
+        {
+            return gestionar.UNIDADES_ALOJAMIENTO.ToList();
+        }
+        public List<RESERVA> devolverReservas()
+        {
+            return gestionar.RESERVAS.ToList();
+        }
+        public RESERVA reservaConcreta(int id)
+        {
+            return gestionar.RESERVAS.Find(id);
+        }
+        public List<PAGO> devolverPagos()
+        {
+            return gestionar.PAGOS.ToList();
+        }
+        public PAGO pagoConcreto(int id)
+        {
+            return gestionar.PAGOS.Find(id);
+        }
+        public void editarReserva(int id, DateTime fechaEntrada, DateTime fechaSalida, int cantidadPersonas, string estado, decimal? fianza, decimal importeEstimado)
+        {
+
+        }
+        public void eliminarReserva(int id)
+        {
+
+        }
+        public void insertarReserva(int idEstablecimiento,int idNumeroUnidad,int idCliente,DateTime fechaEntrada,DateTime fechaSalida,int CantidadPersonas,decimal? fianza,decimal ImporteEstimado)
+        {
+
+        }
+        public void eliminarPago(int id)
+        {
+
+        }
+        public void modificarPago(int idPago,decimal importe,DateTime? fechaPago,string metodoPago)
+        {
+
+        }
+        public void insertarPago(int idReserva, decimal importe, DateTime? FechaPago, string MetodoPago)
+        {
+
+        }
+
+
     }
 }
